d37d63a08c573d89c235a6c72196f4c439fdb2c2
Viktor G. Gumennyy <20798349+sairus7@users.noreply.github.com>
2021-08-10 12:55:18 +0300

    add counter

--- sinc.cpp
+++ sinc.cpp
@@ -15,20 +15,22 @@ int main() {
   int8_t cachebuffer[cache_length];
   uint64_t before = nano();
   size_t howmany = 0;
-
+  size_t i = 0;
   while (std::cin) {
     std::cin.read((char*)cachebuffer, cache_length);
     howmany += std::cin.gcount();
+    i++;
   }
 
   // OR:
   //while (fread(cachebuffer, sizeof(char), cache_length, stdin) != NULL) {
   //  howmany += cache_length;
+  //  i++;
   //}
 
   uint64_t after = nano();
   double giga = howmany / 1000000000.;
-  std::cout << "read " << howmany << " bytes in " << (after - before) << " ns" << std::endl;
+  std::cout << i << " times " << "read " << howmany << " bytes in " << (after - before) << " ns" << std::endl;
   double seconds = (after - before) / 1000000000.;
   double speed = giga / seconds;
   std::cout << speed << " GB/s" << std::endl;
