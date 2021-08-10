058cece78855835ec7d113784f0a6e3fa43461e5
Viktor G <20798349+sairus7@users.noreply.github.com>
2021-08-10 03:09:21 +0300

    pipes v1

---
+++ prog_src/prog_src.cpp
@@ -0,0 +1,27 @@
+﻿// pipes_test.cpp : Defines the entry point for the application.
+//
+//#pragma once
+
+#include <vector>
+#include <cstdint>
+#include <charconv>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+  std::ios_base::sync_with_stdio(false);
+  size_t repeat = 100000;
+  constexpr size_t buflength = 128; //16384;
+  std::vector<char> buffer(buflength, ' ');
+
+  for (size_t i = 0; i < repeat; i++) {
+    std::cout.write(buffer.data(), buflength);
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ script.bat
@@ -0,0 +1,2 @@
+cd .\out\build\x64-Debug\
+.\prog_src\prog_src.exe | .\prog_sinc\prog_sinc.exe
